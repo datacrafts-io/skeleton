6af158b3dda927ea3c1159351b22b72bd3c9b659
Roman Ovcharov <overchind@gmail.com>
2021-08-16 00:45:30 +0300

    Remove require: true for gems in the Gemfile.

--- lib/datacrafts_io_skeleton/tools/gemfiler.rb
+++ lib/datacrafts_io_skeleton/tools/gemfiler.rb
@@ -1,24 +1,15 @@
 module DatacraftsIoSkeleton
   module Tools
     class Gemfiler
-      GEMS_DEFAULT_VALUE = {
-        common: [],
-        development: [],
-        development_test: [],
-        test: []
-      }.freeze
-
-      @gems = GEMS_DEFAULT_VALUE
-      @uncommented_default_gems = []
+      GEMS_DEFAULT_GROUPS = %i[common development development_test test].freeze
 
       class << self
         def clean!
-          @gems = GEMS_DEFAULT_VALUE
-          @uncommented_default_gems = []
+          build_empty_gemset
         end
 
         def add(name, group: :common, required: true)
-          raise "Gem group #{group} is not supported" if @gems[group].nil?
+          raise "Gem group #{group} is not supported" unless GEMS_DEFAULT_GROUPS.include?(group)
 
           @gems[group].push(name: name, required: required)
         end
@@ -31,12 +22,21 @@ module DatacraftsIoSkeleton
 
         def render(group)
           gemfile = @gems[group].map do |gem|
-            "gem '#{gem[:name]}', require: #{gem[:required]}"
+            base = "gem '#{gem[:name]}'"
+            required = gem[:required] == true ? nil : "require: #{gem[:required]}"
+            [base, required].compact.join(", ")
           end
 
           gemfile.join("\n")
         end
+
+        def build_empty_gemset
+          @gems = GEMS_DEFAULT_GROUPS.collect { |group| [group, []] }.to_h
+          @uncommented_default_gems = []
+        end
       end
+
+      build_empty_gemset
     end
   end
 end
